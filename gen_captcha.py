3e501002325576d50ff7a8e4083601c3926c4914
YaoZhang <yaozhang.chn@gmail.com>
2016-07-22 18:36:15 +0800

    fixed memory leak

---
+++ gen_captcha.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 16:46:00 2016
+
+@author: nd
+"""
+
+import random
+
+from PIL import Image
+from PIL import ImageFilter
+from PIL.ImageDraw import Draw
+from PIL.ImageFont import truetype
+
+def random_color(start, end, opacity=None):
+    red = random.randint(start, end)
+    green = random.randint(start, end)
+    blue = random.randint(start, end)
+    if opacity is None:
+        return (red, green, blue)
+    return (red, green, blue, opacity)
+    
+table  =  []
+for  i  in  range( 256 ):
+    table.append( i * 1.97 )
+
+class ImageCaptcha():
+    """Create an image CAPTCHA.
+
+    Many of the codes are borrowed from wheezy.captcha, with a modification
+    for memory and developer friendly.
+
+    ImageCaptcha has one built-in font, DroidSansMono, which is licensed under
+    Apache License 2. You should always use your own fonts::
+
+        captcha = ImageCaptcha(fonts=['/path/to/A.ttf', '/path/to/B.ttf'])
+
+    You can put as many fonts as you like. But be aware of your memory, all of
+    the fonts are loaded into your memory, so keep them a lot, but not too
+    many.
+
+    :param width: The width of the CAPTCHA image.
+    :param height: The height of the CAPTCHA image.
+    :param fonts: Fonts to be used to generate CAPTCHA images.
+    :param font_sizes: Random choose a font size from this parameters.
+    """
+    def __init__(self, width=160, height=60, fonts=None, font_sizes=None):
+        self._width = width
+        self._height = height
+        self._fonts = fonts
+        self._font_sizes = font_sizes or (46, 58, 68)
+        self._truefonts = []
+
+    @property
+    def truefonts(self):
+        if self._truefonts:
+            return self._truefonts
+        self._truefonts = tuple([
+            truetype(n, s)
+            for n in self._fonts
+            for s in self._font_sizes
+        ])
+        return self._truefonts
+
+    @staticmethod
+    def create_noise_curve(image, color):
+        w, h = image.size
+        x1 = random.randint(0, int(w / 5))
+        x2 = random.randint(w - int(w / 5), w)
+        y1 = random.randint(h / 5, h - int(h / 5))
+        y2 = random.randint(y1, h - int(h / 5))
+        points = [(x1, y1), (x2, y2)]
+        end = random.randint(160, 200)
+        start = random.randint(0, 20)
+        Draw(image).arc(points, start, end, fill=color)
+        return image
+
+    @staticmethod
+    def create_noise_dots(image, color, width=3, number=30):
+        draw = Draw(image)
+        w, h = image.size
+        while number:
+            x1 = random.randint(0, w)
+            y1 = random.randint(0, h)
+            draw.line(((x1, y1), (x1 - 1, y1 - 1)), fill=color, width=width)
+            number -= 1
+        return image
+
+    def create_captcha_image(self, chars, color, background):
+        """Create the CAPTCHA image itself.
+
+        :param chars: text to be generated.
+        :param color: color of the text.
+        :param background: color of the background.
+
+        The color should be a tuple of 3 numbers, such as (0, 255, 255).
+        """
+        image = Image.new('RGB', (self._width, self._height), background)
+        draw = Draw(image)
+
+        def _draw_character(c):
+            font = random.choice(self.truefonts)
+            w, h = draw.textsize(c, font=font)
+
+            dx = random.randint(0, 4)
+            dy = random.randint(0, 6)
+            im = Image.new('RGBA', (w + dx, h + dy))
+            Draw(im).text((dx, dy), c, font=font, fill=color)
+
+            # rotate
+            im = im.crop(im.getbbox())
+            im = im.rotate(random.uniform(-30, 30), Image.BILINEAR, expand=1)
+
+            # warp
+            dx = w * random.uniform(0.1, 0.3)
+            dy = h * random.uniform(0.2, 0.3)
+            x1 = int(random.uniform(-dx, dx))
+            y1 = int(random.uniform(-dy, dy))
+            x2 = int(random.uniform(-dx, dx))
+            y2 = int(random.uniform(-dy, dy))
+            w2 = w + abs(x1) + abs(x2)
+            h2 = h + abs(y1) + abs(y2)
+            data = (
+                x1, y1,
+                -x1, h2 - y2,
+                w2 + x2, h2 + y2,
+                w2 - x2, -y1,
+            )
+            im = im.resize((w2, h2))
+            im = im.transform((w, h), Image.QUAD, data)
+            return im
+        
+        images = []
+        for c in chars:
+            images.append(_draw_character(c))
+
+        text_width = sum([im.size[0] for im in images])
+
+        width = max(text_width, self._width)
+        image = image.resize((width, self._height))
+
+        average = int(text_width / len(chars))
+        rand = int(0.25 * average)
+        offset = int(average * 0.1)
+
+        for im in images:
+            w, h = im.size
+            ##########################################
+            #bug fixed  (lambda i:i*1.97 ---> table)
+            ##########################################
+            mask = im.convert('L').point(table) 
+            image.paste(im, (offset, int((self._height - h) / 2)), mask)
+            offset = offset + w + random.randint(-rand, 0)
+        return image
+
+    def generate_image(self, chars):
+        """Generate the image of the given characters.
+
+        :param chars: text to be generated.
+        """
+        background = (255, 255, 255)#random_color(238, 255)
+        color = (0, 0, 0)#random_color(0, 200, random.randint(220, 255))
+        im = self.create_captcha_image(chars, color, background)
+        self.create_noise_dots(im, color)
+        self.create_noise_curve(im, color)
+        im = im.filter(ImageFilter.SMOOTH)
+        return im
+        
+cap = ImageCaptcha(width=80, height=32, fonts=["../OpenSans-Semibold.ttf"], font_sizes = (24,24,24))
+for i in range(10000):
+    im = cap.generate_image("abcd")
